c2cd6a4805710829e13e1d994dd585598d1ee1ab
ladcrane01-glitch <ladcrane01@gmail.com>
2026-02-24 01:38:26 +0100

    Add files via upload

---
+++ jo.js
@@ -0,0 +1,21 @@
+// DARK MODE TOGGLE
+const toggle = document.getElementById("darkToggle");
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+// IMAGE MODAL (LIGHTBOX)
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImage");
+const closeBtn = document.getElementById("closeModal");
+
+document.querySelectorAll(".project-card img").forEach(img => {
+  img.addEventListener("click", () => {
+    modal.style.display = "flex";
+    modalImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
